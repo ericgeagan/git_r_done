e1d1266716a788081340a220ac68f2addebe3ccd
allen <allenfaughn@gmail.com>
2022-05-11 18:22:19 -0500

    Added click listeners to lists.pug

--- public/javascripts/listScript.js
+++ public/javascripts/listScript.js
@@ -1,22 +1,45 @@
 window.addEventListener("load", async () => {
   const lists = document.querySelector(".lists");
-  lists.addEventListener("click", (e) => {
+  const callBack = (e) => {
     e.stopPropagation();
-    const clickedList = e.target.getAttribute("class").slice(9);
-    console.log(clickedList);
+    const clickedListId = e.target.getAttribute("class").slice(9);
+    const clickedList = e.target;
+    console.log(clickedListId);
     async function getAllTasksForList(id) {
-        const pickles = await fetch(`/tasks/list/${clickedList}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
+        const pickles = await fetch(`/tasks/list/${clickedListId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
           },
-        });
-       console.log(pickles);
-    }
-    getAllTasksForList(clickedList);
-  });
+      })
+      .then(response => response.json())
+      .then(data => {
+          const taskdata = data.tasks;
+          console.log(data);
+          console.log(data.tasks);
 
-  console.log("This is working!!!!");
+           taskdata.forEach(task => {
+
+          let tasktdName = document.createElement('td');
+          tasktdName.innerText = task.name;
+          let tasktdCompleted = document.createElement('td');
+          tasktdCompleted.innerText = task.completed;
+          let tasksTable = document.querySelector("#taskTableData");
+          let newRow = document.createElement('tr');
+          tasksTable.appendChild(newRow);
+          newRow.appendChild(tasktdName);
+          newRow.appendChild(tasktdCompleted);
+
+
+      })})
+    }
+    getAllTasksForList(clickedListId)
+    lists.removeEventListener('click', callBack)
+    // console.log(data.tasks);
+};
+lists.addEventListener("click", callBack)
+
+//   console.log("This is working!!!!");
 
   //  try {
   //     const response = await fetch(`/list/${list.id}`, {
