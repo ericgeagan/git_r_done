ee617bc8bbf563c564596ba592279828726f9a72
allen <allenfaughn@gmail.com>
2022-05-11 18:31:22 -0500

    annotated listScript

--- public/javascripts/listScript.js
+++ public/javascripts/listScript.js
@@ -1,10 +1,16 @@
 window.addEventListener("load", async () => {
+
+
+ //Populates Task Table with all tasks respective to that list.
   const lists = document.querySelector(".lists");
+    //Gets the clicked task's Id
   const callBack = (e) => {
     e.stopPropagation();
     const clickedListId = e.target.getAttribute("class").slice(9);
     const clickedList = e.target;
-    console.log(clickedListId);
+    // console.log(clickedListId);
+
+    //Fetches all tasks from the list given an id
     async function getAllTasksForList(id) {
         const pickles = await fetch(`/tasks/list/${clickedListId}`, {
             method: "GET",
@@ -12,13 +18,15 @@ window.addEventListener("load", async () => {
                 "Content-Type": "application/json",
           },
       })
+      //Parses
       .then(response => response.json())
+      //Creates table elements and assigns task data
       .then(data => {
           const taskdata = data.tasks;
-          console.log(data);
-          console.log(data.tasks);
+        //   console.log(data);
+        //   console.log(data.tasks);
 
-           taskdata.forEach(task => {
+          taskdata.forEach(task => {
 
           let tasktdName = document.createElement('td');
           tasktdName.innerText = task.name;
@@ -34,21 +42,13 @@ window.addEventListener("load", async () => {
       })})
     }
     getAllTasksForList(clickedListId)
+
+    //Removes event listeners from tasks that have been clicked
     lists.removeEventListener('click', callBack)
     // console.log(data.tasks);
+
 };
+
+//
 lists.addEventListener("click", callBack)
-
-//   console.log("This is working!!!!");
-
-  //  try {
-  //     const response = await fetch(`/list/${list.id}`, {
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     });
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
 });
